78424bd34439c60905e8c24544f632d6f680b723
Rupam Bora <iamrupam99@gmail.com>
2022-12-10 14:53:35 +0530

    Tag Section and Horizontal News Section Completed

--- lib/main.dart
+++ lib/main.dart
@@ -138,6 +138,148 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
+          const SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            height: 20,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              shrinkWrap:
+                  true, // ***as this list is inside the parent List (Do not use physics-scroll physics) ***
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(
+                    right: 38,
+                  ),
+                  child: Text(
+                    '#Health',
+                    style: kPoppinsMedium.copyWith(
+                      color: kGrey,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 3,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 304,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  padding: const EdgeInsets.all(12),
+                  height: 304,
+                  width: 255,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                    color: kWhite,
+                    boxShadow: [
+                      BoxShadow(
+                        color: kDarkBlue.withOpacity(0.051),
+                        offset: const Offset(0.0, 3.0),
+                        blurRadius: 24.0,
+                        spreadRadius: 0.0,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 164,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(kBorderRadius),
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                              'https://a.cdn-hotels.com/gdcs/production143/d1112/c4fedab1-4041-4db5-9245-97439472cf2c.jpg',
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      Flexible(
+                        child: Text(
+                          'Bali - Unique, unmateched. There is no place like Bali in this world.',
+                          style: kPoppinsBold.copyWith(
+                            fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow
+                              .ellipsis, // gives (...) after overflow
+                        ),
+                      ), // *** To Avoid Overflow***
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 19,
+                                backgroundColor: kLightBlue,
+                                backgroundImage: NetworkImage(
+                                  'https://i0.wp.com/www.xongroh.com/wp-content/uploads/ultimatemember/7/profile_photo-190x190.jpg?ssl=1',
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Rupam Bora",
+                                    style: kPoppinsSemiBold.copyWith(
+                                      fontSize:
+                                          SizeConfig.blockSizeHorizontal! * 3,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Dec 10, 2022",
+                                    style: kPoppinsRegular.copyWith(
+                                      color: kGrey,
+                                      fontSize:
+                                          SizeConfig.blockSizeHorizontal! * 3,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Container(
+                            height: 38,
+                            width: 38,
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.circular(kBorderRadius),
+                              color: kLightWhite,
+                            ),
+                            child: SvgPicture.asset(
+                              'assets/share_icon.svg',
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
